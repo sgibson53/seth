f87503cca18950920e1b5fc1e83ca7cb3fb84e87
Seth Gibson <wgibson53@gmail.com>
2024-10-06 18:20:04 -0500

    added remotes

--- host/src/app/app.routes.ts
+++ host/src/app/app.routes.ts
@@ -3,6 +3,18 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'about',
+    loadChildren: () => import('about/Routes').then((m) => m.remoteRoutes),
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('cart/Routes').then((m) => m.remoteRoutes),
+  },
+  {
+    path: 'shop',
+    loadChildren: () => import('shop/Routes').then((m) => m.remoteRoutes),
+  },
+  {
     path: '',
     component: NxWelcomeComponent,
   },
